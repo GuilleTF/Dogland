7d640b2844d81b472fd66a9b0f8b67122ad5405d
GuilleTF <guilletofe@gmail.com>
2024-10-10 19:16:39 +0200

    arreglo login

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,4 +1,3 @@
-// login_screen.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -6,8 +5,8 @@ import 'package:form_builder_validators/form_builder_validators.dart';
 import 'register_screen.dart';
 import 'forgot_password_screen.dart';
 import '../home_screen.dart';
-import 'legal_info_screen.dart'; // Importa la pantalla de información legal
 import '../../widgets/colors_utils.dart';
+import 'legal_info_screen.dart';
 
 class LoginScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormBuilderState>();
@@ -35,9 +34,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: hexStringToColor("CB2B93"),
-      ),
+      resizeToAvoidBottomInset: true, // Evita el solapamiento del teclado
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
@@ -52,84 +49,88 @@ class LoginScreen extends StatelessWidget {
         ),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: FormBuilder(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/dogland_logo.png',
-                  height: 200,
-                ),
-                SizedBox(height: 20),
-                FormBuilderTextField(
-                  name: 'email',
-                  decoration: InputDecoration(
-                    labelText: 'Correo Electrónico',
-                    labelStyle: TextStyle(color: Colors.white),
+          child: Center(
+            child: FormBuilder(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min, // Evita que el contenido se extienda al fondo
+                children: [
+                  Image.asset(
+                    'assets/images/dogland_logo.png',
+                    height: 200,
                   ),
-                  style: TextStyle(color: Colors.white),
-                  validator: FormBuilderValidators.compose([
-                    FormBuilderValidators.required(),
-                    FormBuilderValidators.email(),
-                  ]),
-                ),
-                FormBuilderTextField(
-                  name: 'password',
-                  decoration: InputDecoration(
-                    labelText: 'Contraseña',
-                    labelStyle: TextStyle(color: Colors.white),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                  obscureText: true,
-                  validator: FormBuilderValidators.required(),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () => _loginUser(context),
-                  child: Text('Iniciar Sesión'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (context) => ForgotPasswordScreen()),
-                    );
-                  },
-                  child: Text(
-                    '¿Olvidaste tu contraseña?',
+                  SizedBox(height: 20),
+                  FormBuilderTextField(
+                    name: 'email',
+                    decoration: InputDecoration(
+                      labelText: 'Correo Electrónico',
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
                     style: TextStyle(color: Colors.white),
+                    validator: FormBuilderValidators.compose([
+                      FormBuilderValidators.required(),
+                      FormBuilderValidators.email(),
+                    ]),
                   ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (context) => RegisterScreen()),
-                    );
-                  },
-                  child: Text(
-                    'Registrarse',
+                  FormBuilderTextField(
+                    name: 'password',
+                    decoration: InputDecoration(
+                      labelText: 'Contraseña',
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
                     style: TextStyle(color: Colors.white),
+                    obscureText: true,
+                    validator: FormBuilderValidators.required(),
                   ),
-                ),
-                // Añade el enlace a la pantalla de términos y condiciones aquí
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                          builder: (context) => LegalInfoScreen()), // La pantalla que creamos para la información legal
-                    );
-                  },
-                  child: Text(
-                    'Términos y Política de Privacidad',
-                    style: TextStyle(
-                      color: Colors.white,
-                      decoration: TextDecoration.underline,
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () => _loginUser(context),
+                    child: Text('Iniciar Sesión'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => ForgotPasswordScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      '¿Olvidaste tu contraseña?',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                ),
-              ],
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => RegisterScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Registrarse',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => LegalInfoScreen(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Términos y Política de Privacidad',
+                      style: TextStyle(
+                        color: Colors.white,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
